294bf65abe976a2ddc35c178b1205605520e0dca
baesparza <bruno.be81@gmail.com>
2018-03-16 23:31:36 -0500

    feat(file): add file manager

--- Primal-core/src/utils/fileutils.h
+++ Primal-core/src/utils/fileutils.h
@@ -3,13 +3,21 @@
 
 namespace primal
 {
-	std::string read_file(const char* filePath);
 
-	class fileutils
+	inline std::string read_file(const char* filePath)
 	{
-	public:
-		fileutils();
-		~fileutils();
-	};
+		FILE* file = fopen(filePath, "rt");
+		fseek(file, 0, SEEK_END);
+		unsigned long length = ftell(file);
+		char* data = new char[length + 1];
+		memset(data, 0, length + 1);
+		fseek(file, 0, SEEK_SET);
+		fread(data, 1, length, file);
+		fclose(file);
+
+		std::string result(data);
+		delete[] data;
+		return result;
+	}
 
 }
