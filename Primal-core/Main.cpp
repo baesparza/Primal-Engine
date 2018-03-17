0ee2aa82c0bd09ca2c8d2227a5d7a05e2680be9f
baesparza <bruno.be81@gmail.com>
2018-03-16 23:48:06 -0500

    feat(Render): add square

--- Primal-core/Main.cpp
+++ Primal-core/Main.cpp
@@ -13,9 +13,23 @@ int main()
 
 	Window window("Primal Engine", 960, 540);
 
-	GLuint vao;
-	glGenVertexArrays(1, &vao);
-	glBindVertexArray(vao);
+	GLfloat vertices[] =
+	{
+		-0.5f, -0.5f,  0.0f, // 0
+		 0.5f, -0.5f,  0.0f, // 1
+		 0.5f,  0.5f,  0.0f, // 2
+
+		 0.5f,  0.5f,  0.0f, // 2
+		-0.5f, -0.5f,  0.0f, // 0
+		-0.5f,  0.5f,  0.0f // 3
+	};
+
+	GLuint vbo;
+	glGenBuffers(1, &vbo);
+	glBindBuffer(GL_ARRAY_BUFFER, vbo);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
+	glEnableVertexAttribArray(0);
 
 	Shader shader("./res/shader/basic.ver", "./res/shader/basic.frag");
 	shader.enable();
@@ -24,7 +38,7 @@ int main()
 	{
 		window.clear();
 
-		glDrawArrays(GL_ARRAY_BUFFER, 0, 6);
+		glDrawArrays(GL_TRIANGLES, 0, 6);
 
 		window.update();
 	}
