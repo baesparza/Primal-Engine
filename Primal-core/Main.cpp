7720c34bef3d4dc62a3b7c8aa6dcb4a91c9fbc75
Bruno Esparza <bruno.be81@gmail.com>
2018-04-17 21:05:44 -0500

    feat(freeImage): add lib and test

--- Primal-core/Main.cpp
+++ Primal-core/Main.cpp
@@ -10,6 +10,9 @@
 #define BATCH_RENDERER 1
 #define TEST_50K_SPRITES 0
 
+#include <FreeImage.h>
+
+#if 0
 int main()
 {
 	using namespace primal;
@@ -83,3 +86,52 @@ int main()
 
 	return 0;
 }
+#else
+
+int main()
+{
+	const char* filename = "test.png";
+
+	//image format
+	FREE_IMAGE_FORMAT fif = FIF_UNKNOWN;
+	//pointer to the image, once loaded
+	FIBITMAP *dib(0);
+	//pointer to the image data
+	BYTE* bits(0);
+	//image width and height
+	unsigned int width(0), height(0);
+	//OpenGL's image ID to map to
+	GLuint gl_texID;
+
+	//check the file signature and deduce its format
+	fif = FreeImage_GetFileType(filename, 0);
+	//if still unknown, try to guess the file format from the file extension
+	if (fif == FIF_UNKNOWN)
+		fif = FreeImage_GetFIFFromFilename(filename);
+	//if still unkown, return failure
+	if (fif == FIF_UNKNOWN)
+		return false;
+
+	//check that the plugin has reading capabilities and load the file
+	if (FreeImage_FIFSupportsReading(fif))
+		dib = FreeImage_Load(fif, filename);
+	//if the image failed to load, return failure
+	if (!dib)
+		return false;
+
+	//retrieve the image data
+	bits = FreeImage_GetBits(dib);
+	//get the image width and height
+	width = FreeImage_GetWidth(dib);
+	height = FreeImage_GetHeight(dib);
+	//if this somehow one of these failed (they shouldn't), return failure
+	if ((bits == 0) || (width == 0) || (height == 0))
+		return false;
+
+	std::cout << width << ", " << height << '\n';
+
+
+	return 0;
+}
+
+#endif
