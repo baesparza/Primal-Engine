0a10f0d0198365685b0ec8679c7e2c82d8c47c3c
Bruno Esparza <bruno.be81@gmail.com>
2018-04-27 09:34:41 -0500

    feat(textue): delete data

--- Primal-core/src/utils/ImageLoad.h
+++ Primal-core/src/utils/ImageLoad.h
@@ -9,7 +9,6 @@ namespace primal
 	{
 		FREE_IMAGE_FORMAT fif = FIF_UNKNOWN;
 		FIBITMAP *dib = nullptr;
-		BYTE* bits = nullptr;
 
 		fif = FreeImage_GetFileType(filename, 0);
 		if (fif == FIF_UNKNOWN)
@@ -22,9 +21,15 @@ namespace primal
 		if (!dib)
 			return false;
 
-		BYTE* result = FreeImage_GetBits(dib);
+		BYTE* pixels = FreeImage_GetBits(dib);
 		(*width) = FreeImage_GetWidth(dib);
 		(*height) = FreeImage_GetHeight(dib);
+
+		int bits = FreeImage_GetBPP(dib);
+		int size = (*width) * (*height) * (bits / 8);
+		BYTE* result = new BYTE[size];
+		memcpy(result, pixels, size);
+		FreeImage_Unload(dib);
 
 		return result;
 	}
