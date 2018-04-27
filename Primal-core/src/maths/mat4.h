03ab26cc68f60de341506878415b71a4134df216
Bruno Esparza <bruno.be81@gmail.com>
2018-04-26 19:44:30 -0500

    feat(mat4): add invert

--- Primal-core/src/maths/mat4.h
+++ Primal-core/src/maths/mat4.h
@@ -31,6 +31,8 @@ namespace primal
 			vec4 multiply(const vec4& other) const;
 			friend vec4 operator*(const mat4& left, const vec4& right);
 
+			mat4& tempert();
+
 			static mat4 orthographic(float left, float right, float bottom, float top, float near, float far);
 			static mat4 perspective(float fov, float aspectRatio, float near, float far);
 
